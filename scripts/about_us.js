28a165f686fdbc547eb97b4b66aa701761412300
Dopeycylender39 <briggsjjacob@gmail.com>
2023-01-17 14:16:21 -0700

    about us/gallery

--- scripts/about_us.js
+++ scripts/about_us.js
@@ -1,6 +1,4 @@
 var menu_items = document.querySelectorAll(".menu");
-
-
 Array.from(menu_items).forEach(function(menu){
   menu.addEventListener("click", function(){
     this.classList.toggle("active")
