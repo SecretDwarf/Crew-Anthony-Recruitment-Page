e138f1f42511b62f067689e7221cc107effafec7
Dopeycylender39 <briggsjjacob@gmail.com>
2023-01-17 12:45:35 -0700

    commit

--- about_us.js
+++
@@ -1,8 +0,0 @@
-var menu_items = document.querySelectorAll(".menu");
-
-
-Array.from(menu_items).forEach(function(menu){
-  menu.addEventListener("click", function(){
-    this.classList.toggle("active")
-  });
-});
--- nav.js
+++
@@ -1,5 +0,0 @@
-// navbar animation
-window.addEventListener("scroll", function(){
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 0);
-})
